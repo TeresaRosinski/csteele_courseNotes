dfa700aba4cc46648b1a79183c0eb822932959ea
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2021-06-28 20:12:51 -0500

    more scorekeeper

--- scoreKeeperCodeAlong/scripts.js
+++ scoreKeeperCodeAlong/scripts.js
@@ -3,18 +3,26 @@ const p2Button = document.querySelector("#p2Button");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 const resetButton = document.querySelector("#reset");
-const winningScoreSelect = document.querySelector("playTo");
+const winningScoreSelect = document.querySelector("#playTo");
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = winningScoreSelect;
 let isGameOver = false;
 
 p1Button.addEventListener("click", function () {
   if (!isGameOver) {
     p1Score += 1;
+    console.log(p1Score);
+    console.log(isGameOver);
+    console.log(winningScore);
     if (p1Score === winningScore) {
       isGameOver = true;
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
+      console.log(p1Score);
+      console.log(isGameOver);
+      console.log(winningScore);
     }
   }
   p1Display.textContent = p1Score;
@@ -25,19 +33,26 @@ p2Button.addEventListener("click", function () {
     p2Score += 1;
     if (p2Score === winningScore) {
       isGameOver = true;
+      p2Display.classList.add("winner");
+      p1Display.classList.add("loser");
     }
   }
   p2Display.textContent = p2Score;
 });
 
-winningScoreSelect.addEventListener("change", function () {
+winningScoreSelect.addEventListener("change", function (e) {
   winningScore = parseInt(this.value);
+  reset();
 });
 
-resetButton.addEventListener("click", function () {
+resetButton.addEventListener("click", reset);
+
+function reset() {
   isGameOver = false;
   p1Score = 0;
   p2Score = 0;
   p1Display.textContent = 0;
   p2Display.textContent = 0;
-});
+  p2Display.classList.remove("winner", "loser");
+  p1Display.classList.remove("winner", "loser");
+}
